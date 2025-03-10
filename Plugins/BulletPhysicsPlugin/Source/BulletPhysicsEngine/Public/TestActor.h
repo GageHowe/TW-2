c2ba83f3416ef5f7701b0aeefb99fc610039ca57
Gage Howe <howe.gaged@gmail.com>
2025-03-10 14:42:55 -0500

    added NetGUID stuff

--- Plugins/BulletPhysicsPlugin/Source/BulletPhysicsEngine/Public/TestActor.h
+++ Plugins/BulletPhysicsPlugin/Source/BulletPhysicsEngine/Public/TestActor.h
@@ -12,9 +12,13 @@
 #include "ThirdParty/BulletPhysicsEngineLibrary/debug/btdebug.h"
 #include "Components/ShapeComponent.h"
 #include <functional>
+
+#include "BlueprintEditor.h"
+#include "Engine/PackageMapClient.h"
 #include "GameFramework/Actor.h"
 #include "Net/UnrealNetwork.h"
 #include "TestActor.generated.h"
+
 
 USTRUCT(BlueprintType)
 struct Ftris
@@ -24,7 +28,6 @@ struct Ftris
 	FVector b;
 	FVector c;
 	FVector d;
-
 };
 
 USTRUCT(BlueprintType)
@@ -39,7 +42,7 @@ struct FBulletPlayerInput
 		bool BoostInput; // 0-1
 
 	UPROPERTY(BlueprintReadWrite, Category = "Physics Networking")
-    	int32 FrameNumber; // Frame number for this input
+    	int32 FrameNumber; // Frame number for this input; is this necessary?
 };
 
 USTRUCT(BlueprintType) // A FBulletObjectState is the instantaneous state of one object in a frame
@@ -48,19 +51,19 @@ struct FBulletObjectState
 	GENERATED_BODY()
 	
 	UPROPERTY(BlueprintReadWrite, Category = "Physics Networking")
-	int32 ObjectID; // Unique ID for the physics object
+	int32 ObjectID; // is this really necessary?
 
 	UPROPERTY(BlueprintReadWrite, Category = "Physics Networking")
-	FTransform Transform; // Transform of the physics object
+	FTransform Transform;
 
 	UPROPERTY(BlueprintReadWrite, Category = "Physics Networking")
-	FVector Velocity;    // Linear velocity of the physics object
+	FVector Velocity;
 
 	UPROPERTY(BlueprintReadWrite, Category = "Physics Networking")
-	FVector AngularVelocity; // Angular velocity of the physics object
+	FVector AngularVelocity;
 
 	UPROPERTY(BlueprintReadWrite, Category = "Physics Networking")
-	int32 FrameNumber; // Frame number for this state
+	int32 FrameNumber;
 };
 
 USTRUCT(BlueprintType) // A FBulletSimulationState is an array of all object states
@@ -82,20 +85,28 @@ class BULLETPHYSICSENGINE_API ATestActor : public AActor
 	GENERATED_BODY()
 	
 public:	
-	// Sets default values for this actor's properties
 	ATestActor();
 
-	// FIFO Buffer of inputs coming into the server
-	TMpscQueue<FBulletPlayerInput> InputBuffer;
-	
-	// History buffer for client-side prediction
-	// UPROPERTY()
-	TArray<FBulletSimulationState> History;
-
-	UFUNCTION(BlueprintCallable)
-	int getNumInHistory()
+	UObject* GetObjectFromNetGUID(FNetworkGUID guid)
 	{
-		return History.Num();
+		UNetDriver* driver = GetWorld()->GetNetDriver();
+		FNetGUIDCache* cache = driver->GuidCache.Get();
+		if (driver && cache) {
+			UObject *obj = cache->GetObjectFromNetGUID(guid, false);
+			return obj;
+		}
+	}
+
+	FNetworkGUID GetNetGUIDFromActor(AActor* actor) const
+	{
+		if (actor && GetWorld()->GetNetDriver())
+		{
+			FNetworkGUID NetGUID = GetWorld()->GetNetDriver()->GuidCache->GetOrAssignNetGUID(actor);
+			return NetGUID;
+		} else
+		{
+			return FNetworkGUID(); // objectID 0
+		}
 	}
 
 	// current simulation state
@@ -106,30 +117,6 @@ public:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Physics Networking")
 	int32 CurrentFrameNumber = 0;	// Global current frame number
 	const float FixedDeltaTime = 1.0f / 60.0f;
-	
-	// UFUNCTION(BlueprintCallable) 	// Add input to the server's input buffer
-	// void AddInputToBuffer(const FPlayerInput& Input);
-	//
-	// UFUNCTION(BlueprintCallable) 	// Process inputs from the buffer at a fixed rate
-	// void ProcessInputBuffer(float DeltaTime);
-	//
-	// UFUNCTION(BlueprintCallable)	// Predict the physics state on the client
-	// void PredictPhysicsState(const FBulletPlayerInput& Input);
-	//
-	// UFUNCTION(BlueprintCallable)	// Reconcile the client's state with the server's state
-	// void ReconcilePhysicsState(const FBulletPhysicsState& ServerState);
-	//
-	// UFUNCTION(BlueprintCallable)	// Rewind and re-simulate physics if necessary
-	// void RewindAndResimulate(int32 FrameNumber);
-	//
-	// UFUNCTION(BlueprintCallable)	// Serialize the physics state for networking
-	// FBulletPhysicsState SerializePhysicsState() const;
-	//
-	// UFUNCTION(BlueprintCallable) 	// Deserialize the physics state from the server
-	// void DeserializePhysicsState(const FBulletPhysicsState& State);
-	//
-	// UFUNCTION(BlueprintCallable)	// Send periodic updates from the server to clients
-	// void SendPhysicsUpdate();
 	
 	// Bullet section
 	// Global objects
@@ -190,15 +177,10 @@ public:
 	// I chose not to use spinning / rolling friction in the end since it had issues!
 
 protected:
-	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
-
 public:	
-	// Called every frame
 	virtual void Tick(float DeltaTime) override;
-
 	void SetupStaticGeometryPhysics(TArray<AActor*> Actors, float Friction, float Restitution);
-
 	UFUNCTION(BlueprintCallable)
 	void activate();
 	UFUNCTION(BlueprintCallable)
@@ -224,31 +206,17 @@ public:
 	void AddTorqueImpulse(int ID, FVector Torque);
 	typedef const std::function<void(btCollisionShape* /*SingleShape*/, const FTransform& /*RelativeXform*/)>& PhysicsGeometryCallback;
 	void ExtractPhysicsGeometry(AActor* Actor, PhysicsGeometryCallback CB);
-
 	btCollisionObject* AddStaticCollision(btCollisionShape* Shape, const FTransform& Transform, float Friction, float Restitution, AActor* Actor);
-
-
 	void ExtractPhysicsGeometry(UStaticMeshComponent* SMC, const FTransform& InvActorXform, PhysicsGeometryCallback CB);
-
 	void ExtractPhysicsGeometry(UShapeComponent* Sc, const FTransform& InvActorXform, PhysicsGeometryCallback CB);
-
 	void ExtractPhysicsGeometry(const FTransform& XformSoFar, UBodySetup* BodySetup, PhysicsGeometryCallback CB);
-
 	btCollisionShape* GetBoxCollisionShape(const FVector& Dimensions);
-
 	btCollisionShape* GetSphereCollisionShape(float Radius);
-
 	btCollisionShape* GetCapsuleCollisionShape(float Radius, float Height);
-
 	btCollisionShape* GetTriangleMeshShape(TArray<FVector> a, TArray<FVector> b, TArray<FVector> c, TArray<FVector> d);
-	
-
 	btCollisionShape* GetConvexHullCollisionShape(UBodySetup* BodySetup, int ConvexIndex, const FVector& Scale);
-
 	const ATestActor::CachedDynamicShapeData& GetCachedDynamicShapeData(AActor* Actor, float Mass);
-
 	btRigidBody* AddRigidBody(AActor* Actor, const ATestActor::CachedDynamicShapeData& ShapeData, float Friction, float Restitution);
-
 	btRigidBody* AddRigidBody(AActor* Actor, btCollisionShape* CollisionShape, btVector3 Inertia, float Mass, float Friction, float Restitution);
 	UFUNCTION(BlueprintCallable)
 	void StepPhysics(float DeltaSeconds, int substeps);
@@ -261,73 +229,6 @@ public:
 	UFUNCTION(BlueprintCallable)
 	void ResetSim();
 
-	// networking code
+	// networking code goes below
 	
-	UFUNCTION(Server, Unreliable, BlueprintCallable)
-	void Server_SendInputsToServer(FBulletPlayerInput input);
-	
-	TMap<int32, btRigidBody*> PhysicsObjects;
-
-	UFUNCTION(BlueprintCallable)
-	bool StatesMatch(const FBulletSimulationState& A, const FBulletSimulationState& B) const;
-	
-	UFUNCTION(BlueprintCallable)
-	void SaveCurrentState();
-	
-	UFUNCTION(BlueprintCallable)
-	void ConsumeInput();
-	
-	// send state to all clients
-	UFUNCTION(NetMulticast, Reliable, BlueprintCallable)
-	void MC_SendPhysicsStateToClient(FBulletSimulationState state);
-	
-	UFUNCTION(BlueprintCallable)
-	void StepPhysicsXFrames(int frames);
-
-	UFUNCTION(BlueprintCallable)
-	void ApplyObjectState(FBulletObjectState& ObjectState, const FBulletObjectState& DesiredState);
-
-	// client corrects to the state of some frame on the server
-	UFUNCTION(BlueprintCallable)
-	void ClientCorrection(FBulletSimulationState state);
-
-
-	// HELPER FUNCTIONS FOR MEMORY AND OBJECT MANAGEMENT
-	
-	void AddBodyToMap(int32 ObjectID, btRigidBody* Body)
-	{
-		PhysicsObjects.Add(ObjectID, Body);
-	}
-
-	void RemoveBodyFromMap(int32 ObjectID)
-	{
-		if (btRigidBody* Body = PhysicsObjects.FindRef(ObjectID))
-		{
-			// Clean up the Bullet rigid body
-			delete Body->getMotionState();
-			delete Body;
-		}
-		PhysicsObjects.Remove(ObjectID);
-	}
-
-	btRigidBody* GetBodyFromMap(int32 ObjectID) const
-	{
-		return PhysicsObjects.FindRef(ObjectID);
-	}
-	
-	virtual void BeginDestroy() override
-	{
-		Super::BeginDestroy();
-
-		// Clean up all Bullet rigid bodies
-		for (auto& Pair : PhysicsObjects)
-		{
-			if (btRigidBody* Body = Pair.Value)
-			{
-				delete Body->getMotionState();
-				delete Body;
-			}
-		}
-		PhysicsObjects.Empty();
-	}
 };
