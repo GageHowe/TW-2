a3ef6eac82b74465da975040929ac233a45d80be
Gage Howe <howe.gaged@gmail.com>
2025-03-14 19:11:01 -0500

    IT'S FIXED

--- Plugins/BulletPhysicsPlugin/Source/BulletPhysicsEngine/Private/TestActor.cpp
+++ Plugins/BulletPhysicsPlugin/Source/BulletPhysicsEngine/Private/TestActor.cpp
@@ -39,7 +39,7 @@ void ATestActor::BeginPlay()
 void ATestActor::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-	StepPhysics(DeltaTime, 0);
+	StepPhysics(DeltaTime, 1);
 	randvar = mt->getRandSeed();
 	if (HasAuthority())
 	{
@@ -107,7 +107,7 @@ void ATestActor::MC_SendStateToClients_Implementation(FBulletSimulationState Ser
 		APlayerController* PC = GetWorld()->GetFirstPlayerController();
 		ATWPlayerController* TWPC = Cast<ATWPlayerController>(PC);
 		if (!TWPC) return; // check validity
-		double offset = TWPC->TimeOffset;
+		double offset = TWPC->RoundTripDelay;
 		int framesToRewind = FMath::RoundToInt(offset / FixedDeltaTime);
 		if (needsResim)
 		// if (debugShouldResim)
@@ -123,11 +123,11 @@ void ATestActor::MC_SendStateToClients_Implementation(FBulletSimulationState Ser
 				// apply local pawn's inputs
 				FTWPlayerInput PastInput = LocalInputBuffer.Get(framesToRewind-i);
 				LocalPawn->ApplyInputs(PastInput);
-				GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Yellow, FString::Printf(TEXT("Got and applied input: %f, %f, %f"), PastInput.MovementInput.X, PastInput.MovementInput.Y, PastInput.MovementInput.Z));
-				UE_LOG(LogTemp, Warning, TEXT("Applied input: %f, %f, %f from position %i"), PastInput.MovementInput.X, PastInput.MovementInput.Y, PastInput.MovementInput.Z, framesToRewind-i );
+				// GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Yellow, FString::Printf(TEXT("Got and applied input: %f, %f, %f"), PastInput.MovementInput.X, PastInput.MovementInput.Y, PastInput.MovementInput.Z));
+				// UE_LOG(LogTemp, Warning, TEXT("Applied input: %f, %f, %f from position %i"), PastInput.MovementInput.X, PastInput.MovementInput.Y, PastInput.MovementInput.Z, framesToRewind-i );
 
 				// step forward
-				StepPhysics(FixedDeltaTime, 0);
+				StepPhysics(FixedDeltaTime, 1);
 			}
 			debugShouldResim = false;
 		}
@@ -589,7 +589,6 @@ btRigidBody* ATestActor::AddRigidBody(AActor* Actor, btCollisionShape* Collision
 	BtRigidBodies.Add(Body);
 
 	return Body;
-	
 }
 
 void ATestActor::StepPhysics(float DeltaSeconds, int substeps)
